3b2233fa360cd1b641d6ed20ba6c61cc416e50d2
Atharv More <atharvmore30@gmail.com>
2024-06-26 15:23:00 +0530

    modified:   index.js 	new file:   public/css/style.css 	deleted:    public/images/1.jpg 	deleted:    public/images/2.jpg 	deleted:    public/images/2.png 	new file:   public/images/img-4.jpeg 	new file:   public/images/img_1.jpeg 	new file:   public/images/img_2.jpeg new file:   public/images/img_3.jpeg 	new file:   public/images/img_5.jpeg 	deleted:    public/style.css 	deleted:    views/home.ejs 	new file:   views/pages/about.ejs new file:   views/pages/home.ejs 	modified:   views/partials/navbar.ejs

--- index.js
+++ index.js
@@ -8,14 +8,25 @@ const port = 3000;
 // Connect to database
 // connectDB();
 
+// Set the view engine to EJS
 app.set('view engine', 'ejs');
 
+// Set the directory for view files
+app.set('views', path.join(__dirname, 'views/pages'));
+
+// Set the directory for static files
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Define routes
 app.get('/', (req, res) => {
   res.render('home');
 });
 
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+
+// Start the server
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
