83e21800212af0b81c0d92e150b33609da95bac0
Atharv More <atharvmore30@gmail.com>
2024-06-30 12:14:11 +0530

    new file:   components/Links.html modified:   index.js 	new file:   models/Contact.js 	new file:   public/css/contact.css 	new file:   public/images/artist.jpeg 	new file:   public/images/background2.jpg 	deleted:    public/images/celebs.jpg 	new file:   public/js/tailwind-config.js modified:   views/pages/about.ejs 	new file:   views/pages/contact.ejs 	modified:   views/pages/home.ejs modified:   views/partials/footer.ejs 	modified:   views/partials/navbar.ejs

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 const express = require('express');
 const path = require('path');
-// const connectDB = require('./config/db');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const Contact = require('./models/Contact');
+
+
 
 const app = express();
 const port = 3000;
+app.use(bodyParser.json());
 
+
+mongoose.connect('mongodb://localhost:27017/Online-Art-Gallery')
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((err) => {
+    console.error('Error connecting to MongoDB', err);
+  });
 
 app.set('view engine', 'ejs');
 
@@ -20,7 +33,28 @@ app.get('/about', (req, res) => {
   res.render('about');
 });
 
-// Start the server
+app.get('/contact', (req, res) => {
+  res.render('contact');
+})
+
+app.post('/contact', async (req, res) => {
+  try {
+    const { name, email, message } = req.body;
+
+    const newContact = new Contact({
+      name,
+      email,
+      message
+    });
+
+    await newContact.save();
+    res.status(200).send('Contact information saved successfully');
+  } catch (error) {
+    res.status(500).send('Error saving contact information');
+  }
+});
+
+
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
